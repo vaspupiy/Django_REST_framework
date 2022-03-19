1f8dbb6f9c65745d28104d72e3514b961bcf9288
Vasiliy <vaspupiy@mail.ru>
2022-03-19 17:06:19 +0300

    checkout

--- ToDoapp/views.py
+++ ToDoapp/views.py
@@ -1,33 +1,15 @@
 from django.shortcuts import render
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
-from ToDoapp.filters import ProjectFilter, TODOFilter
 from ToDoapp.models import Project, TODO
 from ToDoapp.serializers import ProjectModelSerializer, TODOModelSerializer
-
-
-class ProjectLimitOffsetPagination(LimitOffsetPagination):
-    default_limit = 10
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
-    pagination_class = ProjectLimitOffsetPagination
-    filterset_class = ProjectFilter
-
-
-class TODOLimitOffsetPagination(LimitOffsetPagination):
-    default_limit = 20
 
 
 class TODOModelViewSet(ModelViewSet):
-    queryset = TODO.objects.filter(is_active=True)
+    queryset = TODO.objects.all()
     serializer_class = TODOModelSerializer
-    pagination_class = TODOLimitOffsetPagination
-    filterset_class = TODOFilter
-
-    def perform_destroy(self, instance):
-        instance.is_active = False
-        instance.save()
--- Web_service_TODO_notes/urls.py
+++ Web_service_TODO_notes/urls.py
@@ -20,6 +20,7 @@ from rest_framework.routers import DefaultRouter
 from ToDoapp.views import ProjectModelViewSet, TODOModelViewSet
 from users.views import UserModelViewSet
 
+
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
 router.register('projects', ProjectModelViewSet)
@@ -29,5 +30,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-    # path('test/', UserView.as_view()),
 ]
